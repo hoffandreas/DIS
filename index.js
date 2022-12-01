098957e24504fe269120493e1c78ff0ec24eeb74
hoffandreas <91527056+hoffandreas@users.noreply.github.com>
2022-12-01 14:35:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-    res.send('Hi Hoff');
+    res.send('Hi Hoff and this is edited');
 });
 
 app.listen(3000, () => {
